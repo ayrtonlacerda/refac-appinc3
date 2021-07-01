988c717bc77fc94c2d444c5829f4d6149c40fd02
ayrtonlacerda <ayrtonrodrigues.lacerda@gmail.com>
2021-07-01 10:42:10 -0300

    fix eslint

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,8 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  printWidth: 80,
+  tabWidth: 2,
+  semi: false,
   singleQuote: true,
   trailingComma: 'es5',
-};
+  jsxBracketSameLine: false,
+}
