182653bb1b7a15d39d001d245985f9b6889c34a8
paulohlips <paulohfcmendes@gmail.com>
2021-09-02 14:35:15 -0300

    feat/ adicioando keyboard type nos jsons

---
+++ src/repositories/Molequa/mocks_old/mocksmolequa.js
@@ -0,0 +1,232 @@
+export const creche_b = [
+  {
+    label: 'ADMINISTRAÇÃO',
+    screen: 'Administration',
+    subAreas: [
+      'Recepção',
+      'Secretaria/Orientação',
+      'Diretoria',
+      'Sala de Reunião dos Professores',
+      'Circulação',
+      'Almoxarifado',
+      'Banheiro 1',
+      'Banheiro 2',
+      'Área Externa',
+    ],
+    image: 'PLANTA_DO_BLOCO_ADM',
+  },
+  {
+    label: "AREA EXTERNA",
+    screen: "Forms",
+  },
+  {
+    label: 'MULTIUSO',
+    screen: 'Administration',
+    subAreas: [
+      'Sanitário PNE1',
+      'Sanitário 1',
+      'Sanitário PNE2',
+      'Sanitário 2',
+      'Leitura Multiuso',
+      'Laboratório de informática',
+      'Rack',
+      'Cia Tel',
+      'Cia Ele',
+      'Multiuso - Área externa',
+    ],
+    image: 'PLANTA_DO_BLOCO_MULTIUSO',
+  },
+  {
+    label: "PASSARELA",
+    screen: "Forms",
+  },
+  {
+    label: "PATIO COBERTO",
+    screen: "Forms",
+  },
+  {
+    label: 'PEDAGOGIA 1',
+    screen: 'Administration',
+    subAreas: [
+      'Creche 2',
+      'Repouso 1',
+      'Banheiro 1 Creche 2',
+      'Solarium 1',
+      'Fraldário 1',
+      'Creche 1',
+      'Repouso 2',
+      'Fraldário 2',
+      'Solarium 2',
+      'P1 Área Externa',
+    ],
+    image: 'PLANTA_DO_BLOCO_PEDAGOGIA1',
+  },
+  {
+    label: 'PEDAGOGIA 2',
+    screen: 'Administration',
+    subAreas: [
+      'P2 Creche 3.1',
+      'P2 Repouso 1',
+      'P2 Repouso 2',
+      'P2 Creche 3.2',
+      'P2 Solarium 1',
+      'Pré-escola 1',
+      'P2 Solarium 2',
+      'P2 Área externa',
+    ],
+    image: 'PLANTA_DO_BLOCO_PEDAGOGIA2',
+  },
+  {
+    label: 'SERVIÇO',
+    screen: 'Administration',
+    subAreas: [
+      'Área de serviço descoberta',
+      'Depósito',
+      'Cozinha',
+      'Perecíveis',
+      'DML',
+      'Vestiário 01',
+      'Vestiário feminino',
+      'Lavanderia e circulação',
+      'Rouparia',
+      'Lactário',
+      'Caixa em alvenaria para abrigo das bombas de recalque',
+      'Reservatório',
+      'Área externa',
+    ],
+    image: 'PLANTA_DO_BLOCO_SERVICO',
+  },
+]
+
+// creche c
+export const creche_c = [
+  {
+    label: 'Administração',
+    screen: 'Administration',
+    subAreas: [
+      'Banheiro PNE Adulto Feminino',
+      'Banheiro PNE Adulto Masculino',
+      'Administração',
+      'Almoxarifado - Administração',
+      'Professores',
+      'Área Externa - Administração',
+    ],
+    image: 'planta_c_adm',
+  },
+  {
+    label: "Área Externa ",
+    screen: "Administration",
+    subAreas: [
+      "Gás",
+      "Caixa em Alvenaria (Abrigo de Bombas)",
+      "Reservatório",
+      "Layout",
+      "Revestimento",
+    ],
+  },
+  {
+    label: "Pátio Coberto e Refeitório",
+    screen: "Administration",
+    subAreas: ["Pátio Coberto", "Refeitório"],
+  },
+  {
+    label: "Creche I e II",
+    screen: "Administration",
+    subAreas: [
+      "Creche I - Creche C",
+      "Banho - Creche C",
+      "Repouso - Creche C",
+      "Solarium 1 - Creche C",
+      "Creche II - Creche C",
+      "Banheiro Creche II - Creche C",
+      "Solarium II - Creche C",
+      "Área Externa - Creche C",
+    ],
+    image: 'planta_c_crecheIeII',
+
+  },
+/*   {
+    label: 'Vestiário',
+    screen: 'Administration',
+    subAreas: [
+      'Vestiário 1',
+      'Vestiário 2',
+      'Quadra/Depósito',
+      'Quadra/Área Externa',
+    ],
+  }, */
+  {
+    label: 'Serviço ',
+    screen: 'Administration',
+    subAreas: [
+      'Carga e Descarga',
+      'Cozinha - Creche C',
+      'Despensa',
+      'Lavanderia',
+      'DML - Creche C',
+      'Sanitário Feminino',
+      'Sanitário Masculino',
+      'Depósito - Creche C',
+      'Multiuso e Informática',
+      'S. T. Inf',
+      'Copa Funcionários',
+      'Higienização',
+      'Lactário - Creche C',
+      'Área externa - Serviço',
+    ],
+    image: 'planta_c_servico',
+  },
+  /* {
+    label: 'Quadra Coberta',
+    screen: 'Administration',
+    subAreas: ['Quadra', 'Arquibancada', 'Pilares', 'Quadra/Área Externa'],
+  },
+  {
+    label: 'Cobertura',
+    screen: 'Administration',
+    subAreas: ['Coberta'],
+  }, */
+  {
+    label: 'Creche III e Pré-Escola ',
+    screen: 'Administration',
+    subAreas: [
+      'Sanitário Feminino - Pré escola',
+      'Creche III',
+      'Solarium 1 - Pré escola',
+      'Pré-escola',
+      'Solarium 2 - Pré escola',
+      'Sanitário Masculino - Pré escola',
+      'Área externa - Pré escola',
+    ],
+    image: 'planta_c_crecheIII',
+
+  }
+];
+
+
+// QUADRA
+export const quadra_coberta =
+  [
+    {
+      label: "Vestiário",
+      screen: "Administration",
+      subAreas: [
+        "Vestiário 1",
+        "Vestiário 2",
+        "Quadra/Depósito",
+        "Quadra/Área Externa",
+      ],
+      image: 'planta_quadra_vevestiario',
+    },
+    {
+      label: "Quadra Coberta",
+      screen: "Administration",
+      subAreas: ["Quadra", "Arquibancada", "Pilares", "Quadra/Área Externa"],
+    },
+    {
+      label: "Cobertura",
+      screen: "Administration",
+      subAreas: ["Coberta"],
+    },
+  ];
+
